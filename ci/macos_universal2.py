c319f32f406778e3d90cf2b4dbbca40b5ecf6e0c
soonnae <ohjeongmin9@gmail.com>
2025-12-28 22:05:04 +0900

    [ AutoFiC ] 2 malicious code detected!!

--- ci/macos_universal2.py
+++ ci/macos_universal2.py
@@ -31,7 +31,7 @@ platform2 = 'macosx_13_0_arm64'
 
 def _run_cmd(cmd):
     print(f'RUN {cmd}')
-    rv = subprocess.run(cmd, shell=True, capture_output=True, encoding='utf-8')
+    rv = subprocess.run(cmd, shell=False, capture_output=True, encoding='utf-8')
     print(rv.stdout)
     if bool(rv.stderr):
         print(f'STDERR:\n{rv.stderr}')
@@ -52,12 +52,12 @@ def _run_pip_download(cmd):
 
 
 def is_universal2_file(path):
-    rv = _run_cmd(f'lipo -info {path}')
+    rv = _run_cmd(['lipo', '-info', path])
     return not ('Non-fat' in rv.stdout)
 
 
 def is_universal2(package):
-    rv = _run_cmd(f'pip show -f {package}')
+    rv = _run_cmd(['pip', 'show', '-f', package])
     header = True
     for line in rv.stdout.split('\n'):
         line = line.strip()
@@ -80,7 +80,7 @@ def run():
     if not is_universal2_file(sys.executable):
         print(f'WARNING: python executable is not universal2: {sys.executable}')
 
-    _run_cmd('pip install -U "delocate==0.11.0" --report pip_install_delocate.json')
+    _run_cmd(['pip', 'install', '-U', 'delocate==0.11.0', '--report', 'pip_install_delocate.json'])
     with open('pip_install_delocate.json', 'rt') as f:
         delocate_report = json.load(f)
     os.remove('pip_install_delocate.json')
@@ -88,7 +88,7 @@ def run():
     print(f'delocate_packages: {" ".join(delocate_packages)}')
 
     os.system('rm *.whl')
-    rv = _run_cmd('pip list --format json')
+    rv = _run_cmd(['pip', 'list', '--format', 'json'])
     packages = json.loads(rv.stdout)
     for package in packages:
         name = package['name']
@@ -98,23 +98,23 @@ def run():
         elif not is_universal2(name):
             try:
                 print(f'DOWNLOAD {name} universal2 if available')
-                f3 = _run_pip_download(f'pip download --only-binary :all: --platform {platform0} {name}=={version}')
+                f3 = _run_pip_download(['pip', 'download', '--only-binary', ':all:', '--platform', platform0, f'{name}=={version}'])
             except Exception:
                 f3 = None
             if f3 is None or '-none-any' in f3:
                 print(f'PATCH {name}')
-                f1 = _run_pip_download(f'pip download --only-binary :all: --platform {platform1} {name}=={version}')
-                f2 = _run_pip_download(f'pip download --only-binary :all: --platform {platform2} {name}=={version}')
+                f1 = _run_pip_download(['pip', 'download', '--only-binary', ':all:', '--platform', platform1, f'{name}=={version}'])
+                f2 = _run_pip_download(['pip', 'download', '--only-binary', ':all:', '--platform', platform2, f'{name}=={version}'])
                 if 'x86_64' not in f1 or 'arm64' not in f2:
                     raise RuntimeError('Could not find matching files')
-                _run_cmd(f'delocate-fuse {f1} {f2} -w .')
+                _run_cmd(['delocate-fuse', f1, f2, '-w', '.'])
                 f3 = f2.replace('arm64', 'universal2')
                 os.rename(f1, f3)
-            _run_cmd(f'pip install --force-reinstall -f . {f3}')
+            _run_cmd(['pip', 'install', '--force-reinstall', '-f', '.', f3])
         else:
             print(f'SKIP {name} already universal2')
 
-    _run_cmd(f'pip uninstall -y {" ".join(delocate_packages)}')
+    _run_cmd(['pip', 'uninstall', '-y'] + delocate_packages)
     return 0
 
 
